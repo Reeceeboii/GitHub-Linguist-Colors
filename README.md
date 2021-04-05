4457d963ef17ec553fec05dc02fd46c6892ca073
Reece Mercer <reecemercer981@gmail.com>
2021-04-05 04:33:40 +0100

    Updated readme and tests

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# github-linguist-colours
-Given a string denoting a programming language, return its corresponding GitHub linguist hex colour
+# GitHub Linguist Colours
+Given a string denoting a programming language, return its corresponding GitHub linguist hex colour:
+
+
+```javascript
+const linguist = require('linguist');
+```
+
+```javascript
+linguist.GetHexColour('JavaScript');
+
+>#f1e05a
+
+linguist.GetHexColour('Swift');
+
+>#ffac45
+```
+
+```js
+linguist.GetColourMap();
+>
+  {
+  ...
+  'graphql' => '#e10098',
+  'groovy' => '#e69f56',
+  'html' => '#e34c26',
+  'hack' => '#878787',
+  'haml' => '#ece2a9',
+  'handlebars' => '#f7931e',
+  'harbour' => '#0e60e3',
+  'haskell' => '#5e5086',
+  'haxe' => '#df7900',
+  'hiveql' => '#dce200',
+  'holyc' => '#ffefaf',
+  ...
+  }
+
+```
--- test/linguist.test.js
+++ test/linguist.test.js
@@ -1,14 +1,24 @@
-// eslint-disable-next-line no-unused-vars
 const linguist = require('../dist/linguist');
 
-test('GetHexColour() uppercase', () => {
+// eslint-disable-next-line no-console
+console.log(linguist.GetColourMap());
+
+test('GetHexColour() JS uppercase', () => {
   expect(linguist.GetHexColour('JavaScript')).toBe('#f1e05a');
 });
 
-test('GetHexColour() lowercase', () => {
+test('GetHexColour() JS lowercase', () => {
   expect(linguist.GetHexColour('javascript')).toBe('#f1e05a');
 });
 
-test('GetColourMap()', () => {
+test('GetHexColour() Swift lowercase', () => {
+  expect(linguist.GetHexColour('swift')).toBe('#ffac45');
+});
+
+test('GetHexColour() unknown language', () => {
+  expect(linguist.GetHexColour('unobtainium')).toBe('#000');
+});
+
+test('GetColourMap() returns Map', () => {
   expect(linguist.GetColourMap() instanceof Map).toBe(true);
 });
